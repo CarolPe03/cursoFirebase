d1ff1c096837470ab29a75f824e277b63f1c2f09
CarolPe03 <anacarolpc11@gmail.com>
2023-06-10 14:35:52 -0600

    Navbar

---
+++ generic.js
@@ -0,0 +1,12 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyB0ytTY3TLna2vILvT2WzyOspjlTl3P11Y",
+    authDomain: "cursofirebase-adba9.firebaseapp.com",
+    projectId: "cursofirebase-adba9",
+    storageBucket: "cursofirebase-adba9.appspot.com",
+    messagingSenderId: "234912305790",
+    appId: "1:234912305790:web:77989a7e17da42a4933e2f",
+    measurementId: "G-Q3P18VVJNQ"
+  };
+
+  firebase.initializeApp(firebaseConfig);

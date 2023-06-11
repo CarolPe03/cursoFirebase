27e00901cf4555ab8baa545355cad456cca2140f
CarolPe03 <anacarolpc11@gmail.com>
2023-06-11 13:48:03 -0600

    Signin-IniciarSesion

--- js/index.js
+++ js/index.js
@@ -1,5 +1,25 @@
-function createUser(){
+function iniciarSesion() {
+    var email = document.getElementById("txtcorreoIngresar").value;
+    var password = document.getElementById("txtcotraIngresar").value;
 
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(res => {
+            document.location.href="./misPrestamos.html";
+            //Imagen
+            if (res.user.photoURL!=null) {
+               document.getElementById(imgFotoUsuario).src=res.user.photoURL;
+            }else{
+                document.getElementById(imgFotoUsuario).src="./img/noUsuario.png";
+            }
+        })
+        .catch(err => {
+          console.error(err);
+          document.getElementById("alertErrorLogueo").style.display="block";
+          document.getElementById("alertErrorLogueo").innerHTML=err;
+        });
+}
+
+function createUser(){
     var email= document.getElementById("txtcorreo").value;
     var password= document.getElementById("txtcontra").value;
     firebase.auth().createUserWithEmailAndPassword(email,password)
